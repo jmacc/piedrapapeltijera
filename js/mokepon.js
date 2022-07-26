f326c80cd74aa04a6035391c8165483ee081b405
jmacc <j2macc@gmail.com>
2022-07-26 12:26:28 -0500

    modificar datos del dom

--- js/mokepon.js
+++ js/mokepon.js
@@ -3,6 +3,10 @@ function iniciarJuego() {
     botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
 
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
 function seleccionarMascotaJugador() {
     let inputHipoge = document.getElementById('Hipoge')
     let inputCapipepo = document.getElementById('Capipepo')
@@ -10,20 +14,26 @@ function seleccionarMascotaJugador() {
     let inputLangostelvis = document.getElementById('Langostelvis')
     let inputTucapalma= document.getElementById('Tucapalma')
     let inputPydos = document.getElementById('Pydos')
+    let spanMascotaJuagador = document.getElementById('mascota-jugador')
 
     if (inputHipoge.checked) {
-        alert('Seleccionaste Hipoge')
+       // alert('Seleccionaste Hipoge')
+       spanMascotaJuagador.innerHTML = 'Hipoge'
     } else if (inputCapipepo.checked) {
-        alert('Seleccionaste Capipepo')
-    }else if (inputRatihueya.checked) {
-        alert('Seleccionaste Ratihueya')
-    }else if (inputLangostelvis.checked) {
-        alert('Seleccionaste Langostelvis')
-    } if (inputTucapalma.checked) {
-        alert('Seleccionaste Tucapalma')
-    }
-     if (inputPydos.checked) {
-        alert('Seleccionaste Pydos')
+       // alert('Seleccionaste Capipepo')
+       spanMascotaJuagador.innerHTML = 'Capipepo'
+    } else if (inputRatihueya.checked) {
+        //alert('Seleccionaste Ratihueya')
+        spanMascotaJuagador.innerHTML = 'Ratihueya'
+    } else if (inputLangostelvis.checked) {
+        //alert('Seleccionaste Langostelvis')
+        spanMascotaJuagador.innerHTML = 'Langostelvis'
+    } else if (inputTucapalma.checked) {
+        //alert('Seleccionaste Tucapalma')
+        spanMascotaJuagador.innerHTML = 'Tucapalma'
+    } else if (inputPydos.checked) {
+        //alert('Seleccionaste Pydos')
+        spanMascotaJuagador.innerHTML = 'Pydos'
     } else  {
         alert('Selecciona una mascota')
     }
